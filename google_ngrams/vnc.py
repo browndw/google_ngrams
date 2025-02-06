48b11921af2e974cb2d08ec7f462fbc825c9fffd
browndw <davidwestbrown@gmail.com>
2025-02-06 08:36:03 -0500

    Update vnc.py

--- google_ngrams/vnc.py
+++ google_ngrams/vnc.py
@@ -245,6 +245,424 @@ def _linkage_matrix(time_series,
     return hc
 
 
+def _find_first_previous_match(arr, value):
+    """Finds the index of the first previous match in an array."""
+    indices = np.where(arr == value)[0]
+    if len(indices) > 0:
+        return indices[0].item()
+    else:
+        np.nan
+
+
+def _update_centers(df, left_matches, right_matches):
+    centers = (
+        df.sort("dcoord_2", descending=False)
+        .with_columns(centers=pl.mean_horizontal(["icoord_2", "icoord_3"]))
+        .select("centers").to_series().to_list()
+    )
+    centers_left = np.array(
+        [
+            centers[left_matches[i]]
+            if left_matches[i] is not None
+            else np.nan for i in range(len(left_matches))
+        ], dtype=np.float64)
+    centers_right = np.array(
+        [
+            centers[right_matches[i]]
+            if right_matches[i] is not None
+            else np.nan for i in range(len(right_matches))
+            ], dtype=np.float64)
+
+    df = (df.with_columns(
+            centers_left=centers_left
+        )
+        .with_columns(centers_right=centers_right)
+        .with_columns(
+            pl.when(pl.col("icoord_2").is_nan())
+            .then(pl.col("centers_left"))
+            .otherwise(pl.col("icoord_2")).alias("icoord_2")
+        )
+        .with_columns(
+            pl.when(pl.col("icoord_3").is_nan())
+            .then(pl.col("centers_right"))
+            .otherwise(pl.col("icoord_3")).alias("icoord_3")
+        )
+    )
+    return df
+
+
+def _reorder_leaves(R):
+
+    # get list of leaves by index
+    ivl_list = R["leaves"]
+    color_list = R["color_list"]
+
+    # place the dendrogram coordinates in a dataframe
+    coord_df = pl.DataFrame(
+        {'icoord': R['icoord'], 'dcoord': R['dcoord']}
+        ).with_row_index()
+
+    # unnest the coorinates and sort in order of their placement left-to-right
+    coord_df = (coord_df
+                .with_columns(
+                    pl.col('icoord').list.to_struct(fields=[
+                        "icoord_1", "icoord_2", "icoord_3", "icoord_4"]))
+                .with_columns(
+                    pl.col('dcoord').list.to_struct(fields=[
+                        "dcoord_1", "dcoord_2", "dcoord_3", "dcoord_4"]))
+                .unnest('icoord')
+                .unnest('dcoord')
+                ).sort(pl.col("icoord_1"))
+
+    # indicate which leaves are singletons and witch are in pairs
+    coord_df = (coord_df
+                .with_columns(
+                    pl.when(
+                        dcoord_1=0, dcoord_4=0
+                        ).then(2).otherwise(None).alias("n_leaves")
+                )
+                .with_columns(
+                    pl.when(
+                        (pl.col("n_leaves").is_null() &
+                            pl.col("dcoord_1").eq(0))
+                            ).then(1).otherwise(
+                                pl.col("n_leaves")).alias("n_leaves"))
+                .with_columns(
+                    pl.when(
+                        (pl.col("n_leaves").is_null() &
+                            pl.col("dcoord_4").eq(0))).then(1).otherwise(
+                                pl.col("n_leaves")).alias("n_leaves")))
+
+    # create indices for groupings
+    coord_df = (coord_df
+                .with_columns(
+                    pl.col("n_leaves").cum_sum().sub(2).alias("idx_1")
+                )
+                .with_columns(
+                    pl.when(pl.col("n_leaves").eq(2)).then(1).alias("idx_2")
+                )
+                .with_columns(
+                    pl.col("idx_2").add(pl.col("idx_1"))
+                )
+                .with_columns(
+                    pl.when(
+                        pl.col("idx_1").is_not_null() &
+                        pl.col("idx_2").is_null()
+                        ).then(pl.col("idx_1").add(1)
+                                ).otherwise(pl.col("idx_1"))
+                )
+                ).drop("n_leaves")
+
+    coord_df = (coord_df
+                .with_columns(ivl_list=ivl_list)
+                .with_columns(
+                    ivl_1=pl.col("ivl_list").list.slice(
+                        pl.col("idx_1"), length=1).list.first().cast(pl.Int32))
+                .with_columns(
+                    ivl_2=pl.col("ivl_list").list.slice(
+                        pl.col("idx_2"), length=1).list.first().cast(pl.Int32))
+                )
+
+    # based on the indices determine how far the iccord of leaves that
+    # have icoord_1 or icoord_4 of 0 need to moved to follow
+    # the sequential order indicated by ivl integers
+    coord_df = (coord_df
+                .with_columns(leaf_idx=pl.col("ivl_1").forward_fill())
+                .with_columns(
+                    pl.col("leaf_idx").shift(-1).alias("next_idx")
+                ).with_columns(
+                    pl.when(
+                        pl.col("next_idx").ne(pl.col("leaf_idx"))
+                        ).then(pl.col("next_idx")
+                                ).otherwise(None).backward_fill()
+                    )
+                .with_columns(
+                    pl.col("leaf_idx").sub(pl.col("idx_1"))
+                    .alias("x_adjust").forward_fill()
+                    )
+                .with_columns(pl.col("x_adjust").mul(10))
+                ).drop("ivl_list")
+
+    # sort by the heights the y-axis so links are ordered
+    # bottom to top as indicated by dcoord_2
+    coord_df = (coord_df.sort("dcoord_2", descending=False)
+                .with_columns(
+                    pl.when(
+                        pl.col("dcoord_1").eq(0))
+                .then(pl.col(["icoord_1", "icoord_2"]).add(pl.col("x_adjust")))
+                .otherwise(np.nan)
+                )
+                .with_columns(
+                pl.when(
+                    pl.col("dcoord_4").eq(0))
+                .then(pl.col(["icoord_3", "icoord_4"]).add(pl.col("x_adjust")))
+                .otherwise(np.nan)
+                )
+                )
+
+    # map linkages by finding the nearest linkages left and right
+    # down the dendrogam as indicated by their shared coordinates
+    dcoord_1 = coord_df.sort(
+        "dcoord_2", descending=False)["dcoord_1"].to_numpy()
+    dcoord_2 = coord_df.sort(
+        "dcoord_2", descending=False)["dcoord_2"].to_numpy()
+
+    dcoord_3 = coord_df.sort(
+        "dcoord_2", descending=False)["dcoord_3"].to_numpy()
+    dcoord_4 = coord_df.sort(
+        "dcoord_2", descending=False)["dcoord_4"].to_numpy()
+
+    left_matches = []
+    for i in range(len(dcoord_1)):
+        match = _find_first_previous_match(dcoord_2, dcoord_1[i])
+        left_matches.append(match)
+
+    right_matches = []
+    for i in range(len(dcoord_1)):
+        match = _find_first_previous_match(dcoord_3, dcoord_4[i])
+        right_matches.append(match)
+
+    # calculate how much spans need to be shifted
+    # along the x-axis by iteratvily updating the 
+    # centers from the bottom to the top 
+    df_list = [coord_df]
+
+    for i in range(1, len(right_matches)):
+        df = df_list[-1]
+        df = _update_centers(df, left_matches, right_matches)
+        df_list.append(df)
+
+    # take the last iteration
+    coord_df = (
+        df_list[-1]
+        .with_columns(pl.col("icoord_2").alias("icoord_1"))
+        .with_columns(pl.col("icoord_3").alias("icoord_4"))
+        .sort("icoord_1")
+        )
+
+    # adjust centers to their final position
+    coord_df = (
+        coord_df
+        .with_columns(
+                    pl.when(
+                        (pl.col("icoord_3") != pl.col("centers_right")) &
+                        pl.col("centers_right").is_not_nan()
+                        ).then(pl.col("centers_right")
+                                ).otherwise(pl.col("icoord_3")).alias("icoord_3")
+                )
+        .with_columns(
+                    pl.when((pl.col("icoord_2") != pl.col("centers_left")) &
+                            pl.col("centers_left").is_not_nan()
+                            ).then(pl.col("centers_left")
+                                    ).otherwise(pl.col("icoord_2")
+                                                ).alias("icoord_2")
+                )
+        .with_columns(
+                    pl.when(
+                        (pl.col("icoord_4") != pl.col("centers_right"))
+                        & pl.col("centers_right").is_not_nan()
+                        ).then(pl.col("centers_right")
+                                ).otherwise(pl.col("icoord_4")
+                                            ).alias("icoord_4")
+                )
+        .with_columns(
+            pl.when(
+                (pl.col("icoord_1") != pl.col("centers_left")) &
+                pl.col("centers_left").is_not_nan()
+                ).then(pl.col("centers_left")
+                        ).otherwise(pl.col("icoord_1")
+                                    ).alias("icoord_1")
+                        )
+            )
+
+    # select columns needed for the updated dendrogram dictionary
+    X = (
+        coord_df
+        .with_columns(
+                pl.concat_list(pl.selectors.starts_with("icoord_"))
+                .alias('icoord')
+                )
+        .with_columns(
+                pl.concat_list(pl.selectors.starts_with("dcoord_")).alias('dcoord')
+                )
+        .with_columns(
+                pl.concat_list(pl.selectors.starts_with("ivl_")).alias('ivl')
+                ).select("icoord", "dcoord", "ivl")
+            )
+
+    # put ivl data into list droping null values
+    ivl = [
+        x for xs in X.get_column("ivl").to_list() for x in xs if x is not None
+        ]
+
+    # format the final dictionary
+    X = {"icoord": X.get_column("icoord").to_list(),
+            "dcoord": X.get_column("dcoord").to_list(),
+            "ivl": ivl,
+            "color_list": color_list}
+
+    return X
+
+
+def _vnc_dendrogram(Z,
+                    p=30,
+                    truncate_mode=None,
+                    color_threshold=0,
+                    get_leaves=True,
+                    orientation='top',
+                    labels=None,
+                    count_sort=False,
+                    distance_sort=False,
+                    show_leaf_counts=True,
+                    no_plot=True,
+                    no_labels=False,
+                    leaf_font_size=None,
+                    leaf_rotation=None,
+                    leaf_label_func=None,
+                    show_contracted=False,
+                    link_color_func=None,
+                    ax=None,
+                    above_threshold_color='k'):
+    """
+    This is a workaround to access the contraction marks
+    to allow for customized plotting.
+    """
+    xp = sch.array_namespace(Z)
+    Z = sch._asarray(Z, order='c', xp=xp)
+
+    if orientation not in ["top", "left", "bottom", "right"]:
+        raise ValueError("orientation must be one of 'top', 'left', "
+                            "'bottom', or 'right'")
+
+    if labels is not None:
+        try:
+            len_labels = len(labels)
+        except (TypeError, AttributeError):
+            len_labels = labels.shape[0]
+        if Z.shape[0] + 1 != len_labels:
+            raise ValueError("Dimensions of Z and labels must be consistent.")
+
+    sch.is_valid_linkage(Z, throw=True, name='Z')
+    Zs = Z.shape
+    n = Zs[0] + 1
+    if isinstance(p, (int, float)):
+        p = int(p)
+    else:
+        raise TypeError('The second argument must be a number')
+
+    if truncate_mode not in ('lastp', 'mtica', 'level', 'none', None):
+        # 'mtica' is kept working for backwards compat.
+        raise ValueError('Invalid truncation mode.')
+
+    if truncate_mode == 'lastp':
+        if p > n or p == 0:
+            p = n
+
+    if truncate_mode == 'mtica':
+        # 'mtica' is an alias
+        truncate_mode = 'level'
+
+    if truncate_mode == 'level':
+        if p <= 0:
+            p = np.inf
+
+    if get_leaves:
+        lvs = []
+    else:
+        lvs = None
+
+    icoord_list = []
+    dcoord_list = []
+    color_list = []
+    current_color = [0]
+    currently_below_threshold = [False]
+    ivl = []  # list of leaves
+
+    if color_threshold is None or (isinstance(color_threshold, str) and
+                                    color_threshold == 'default'):
+        color_threshold = xp.max(Z[:, 2]) * 0.7
+
+    R = {'icoord': icoord_list, 'dcoord': dcoord_list, 'ivl': ivl,
+            'leaves': lvs, 'color_list': color_list}
+
+    # Empty list will be filled in _dendrogram_calculate_info
+    contraction_marks = [] if show_contracted else None
+    clusters = [] if truncate_mode is not None else None
+
+    sch._dendrogram_calculate_info(
+        Z=Z, p=p,
+        truncate_mode=truncate_mode,
+        color_threshold=color_threshold,
+        get_leaves=get_leaves,
+        orientation=orientation,
+        labels=labels,
+        count_sort=count_sort,
+        distance_sort=distance_sort,
+        show_leaf_counts=show_leaf_counts,
+        i=2*n - 2,
+        iv=0.0,
+        ivl=ivl,
+        n=n,
+        icoord_list=icoord_list,
+        dcoord_list=dcoord_list,
+        lvs=lvs,
+        current_color=current_color,
+        color_list=color_list,
+        currently_below_threshold=currently_below_threshold,
+        leaf_label_func=leaf_label_func,
+        contraction_marks=contraction_marks,
+        link_color_func=link_color_func,
+        above_threshold_color=above_threshold_color)
+
+    if not no_plot:
+        mh = xp.max(Z[:, 2])
+        sch._plot_dendrogram(icoord_list, dcoord_list, ivl, p, n, mh, orientation,
+                            no_labels, color_list,
+                            leaf_font_size=leaf_font_size,
+                            leaf_rotation=leaf_rotation,
+                            contraction_marks=contraction_marks,
+                            ax=ax,
+                            above_threshold_color=above_threshold_color)
+
+    R["leaves_color_list"] = sch._get_leaves_color_list(R)
+
+    if truncate_mode is None:
+        R = _reorder_leaves(R)
+
+    if truncate_mode == 'lastp':
+        R2 = sch.dendrogram(Z, no_plot=True)
+        clusters = R['ivl']
+        clusters_assignment = []
+        for i in range(len(clusters)):
+            if clusters[i].startswith("("):
+                n = clusters[i].removeprefix("(").removesuffix(")")
+                n = int(n)
+                clusters_assignment.append([i] * n)
+            else:
+                clusters_assignment.append([i])
+        clusters_assignment = [x for xs in clusters_assignment for x in xs]
+        df_clst = pl.DataFrame(
+            {'index': R2["leaves"], 'cluster': clusters_assignment}
+            ).sort("index")
+        df_clst = df_clst.with_columns(
+            cluster_min=pl.col("index").min().over(pl.col("cluster"))
+            ).with_columns(
+            size=pl.col("index").count().over(pl.col("cluster"))
+            ).with_columns(
+            cluster_max=pl.col("index").max().over(pl.col("cluster"))
+            )
+        clusters = df_clst.to_numpy(structured=True)
+
+    mh = xp.max(Z[:, 2])
+    R['n'] = n
+    R['mh'] = mh
+    R['p'] = p
+    R['clusters'] = clusters
+    R["contraction_marks"] = contraction_marks
+
+    return R
+
+
 class TimeSeries:
 
     def __init__(self,
